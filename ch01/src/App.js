4bc5f29a3aa5000cdcc7085867df485cca17e94a
junhyeokkk <loveu9911@naver.com>
2024-11-11 09:49:45 +0900

    react ch01

---
+++ ch01/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h3> Hello World</h3>
+      <h4> Hello React!</h4>
+    </div>
+  );
+}
+
+export default App;
